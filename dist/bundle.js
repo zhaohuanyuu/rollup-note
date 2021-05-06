5fe4bf79a5bc57497bdeadf1f300aeb42cfb258a
zhaohuanyu <zhaohuanyu@aliyun.com>
2021-05-06 16:37:06 +0800

    add simple demo

---
+++ dist/bundle.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const greet = function () {
+	console.log('greet');
+};
+
+greet();
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const greet = function () {
+	console.log('greet');
+}
+
+const foo = function() {
+	console.log('foo');
+}
+
+export {
+	foo,
+	greet
+};
